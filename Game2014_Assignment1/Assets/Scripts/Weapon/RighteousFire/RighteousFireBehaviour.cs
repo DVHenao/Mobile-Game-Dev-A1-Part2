e63aedb497d9786bb8b44372ffc15911f2e57b58
DVHenao <ehenao094@gmail.com>
2023-11-04 22:42:05 -0400

    added melee weapon category

---
+++ Game2014_Assignment1/Assets/Scripts/Weapon/RighteousFire/RighteousFireBehaviour.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RighteousFireBehaviour : MeleeWeaponBehaviour
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+}
